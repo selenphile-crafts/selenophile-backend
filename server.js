bc168a5abeb2668d414ce51fa39916a9c3fae998
Sher Mohammad <msher4560@gmail.com>
2026-05-19 14:38:56 +0530

    CROS update

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://achievers-library.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB connection
